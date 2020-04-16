4deb6c9e64e31db74321d82e424e3a8eb00e351e
Ridhi <ridhisurat@gmail.com>
2020-04-16 20:50:37 +0530

    Ridhi added

---
+++ NewTrial/src/home/prac/coding/Mytesting.java
@@ -0,0 +1,14 @@
+package home.prac.coding;
+
+public class Mytesting {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Surat Here");
+		// TODO Auto-generated method stub
+
+	}
+
+}
