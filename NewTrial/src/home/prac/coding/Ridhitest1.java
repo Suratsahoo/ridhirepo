fe7e2bfeba01e90d9be99631168796bb5660995d
Ridhi <ridhisurat@gmail.com>
2020-04-16 20:58:25 +0530

    Ridhi added Ridhitest1.java

---
+++ NewTrial/src/home/prac/coding/Ridhitest1.java
@@ -0,0 +1,14 @@
+package home.prac.coding;
+
+public class Ridhitest1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello all hehe");
+		// TODO Auto-generated method stub
+
+	}
+
+}
